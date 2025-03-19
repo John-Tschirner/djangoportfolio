625b1910aeb15dbebf2f96fedcff8b96599d53b7
John-Tschirner <johnsatterfield88@gmail.com>
2025-03-19 14:35:04 -0600

    Versioning on sprint01 branch

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -128,3 +128,17 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+
+STATIC_URL = 'static/'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "portfolio/static"),
+]
+
+
+MEDIA_URL = '/images/'
